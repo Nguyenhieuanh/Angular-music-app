157b4643009820420c6bdf649c8311dc156b6fbf
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-24 14:56:12 +0700

    created components youtube-playlist youtube-player

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { YoutubePlaylistComponent } from './components/youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './components/youtube-player/youtube-player.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent,
+    children: [{
+      path: ':id',
+      component: YoutubePlayerComponent
+    }]
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
