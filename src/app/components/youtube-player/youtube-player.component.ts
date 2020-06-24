287d7f37170f950bb8d9bda1e56434f18e26d5e2
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-24 15:00:50 +0700

    update components youtube-playlist, youtube-player

--- src/app/components/youtube-player/youtube-player.component.ts
+++ src/app/components/youtube-player/youtube-player.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ParamMap, ActivatedRoute } from '@angular/router';
+import { YoutubeService } from 'src/app/services/youtube.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-youtube-player',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./youtube-player.component.css']
 })
 export class YoutubePlayerComponent implements OnInit {
+  song: any;
+  sub: Subscription;
+  constructor(
+    private youtubeService: YoutubeService,
+    private activatedRouter: ActivatedRoute,
+    private domSanitizer: DomSanitizer
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.sub = this.activatedRouter.paramMap.subscribe((paramMap: ParamMap) => {
+      const id = paramMap.get('id');
+      this.song = this.youtubeService.find(id);
+    });
+  }
 
-  ngOnInit(): void {
+  getSrc() {
+    const url = 'https://www.youtube.com/embed/' + this.song.id;
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
--- src/app/components/youtube-playlist/youtube-playlist.component.ts
+++ src/app/components/youtube-playlist/youtube-playlist.component.ts
@@ -1,3 +1,4 @@
+import { YoutubeService } from './../../services/youtube.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YoutubePlaylistComponent implements OnInit {
 
-  constructor() { }
+  constructor(public youtubeService: YoutubeService) { }
 
   ngOnInit(): void {
   }
